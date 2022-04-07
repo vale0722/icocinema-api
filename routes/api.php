67c4162a017a296f4d3cc6024977b2c0431ca946
rainymarcelo <chelotdp@gmail.com>
2022-04-06 19:57:21 -0500

    Routes login

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\MoviesController;
 use App\Http\Controllers\Api\RoomsController;
 use App\Http\Controllers\Api\ShowsController;
 use App\Http\Controllers\Api\UsersController;
+use App\Http\Controllers\Auth\ApiAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,12 @@ Route::apiResource('chair', ChairsController::class);
 Route::apiResource('user', UsersController::class);
 Route::apiResource('booking', BookingsController::class);
 
+Route::middleware(['auth:sanctum', 'cors', 'json.response'])->group(function () {
+    Route::get('/me', [ApiAuthController::class, 'me'])->name('auth.me');
+    Route::post('/logout', [ApiAuthController::class, 'logout'])->name('logout.api');
+});
+
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    Route::post('/login', [ApiAuthController::class, 'login'])->name('login.api');
+    Route::post('/register', [ApiAuthController::class, 'register'])->name('register.api');
+});
